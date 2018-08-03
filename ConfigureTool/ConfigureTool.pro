be279dcc206016617248cc7e6cb6c4878630cdce
eddiechen <chenyouzhi06@163.com>
2018-08-03 15:36:31 +0800

    更新ConfigureTool

--- ConfigureTool/ConfigureTool.pro
+++ ConfigureTool/ConfigureTool.pro
@@ -11,7 +11,8 @@ TEMPLATE = app
 
 SOURCES += main.cpp \
     MainWindow.cpp \
-    XML.cpp
+    XML.cpp \
+    CTabMain.cpp
 
 # The following define makes your compiler emit warnings if you use
 # any feature of Qt which as been marked deprecated (the exact warnings
@@ -26,4 +27,8 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 HEADERS += \
     MainWindow.h \
-    XML.h
+    XML.h \
+    CTabMain.h
+
+RESOURCES += \
+    source.qrc
--- ConfigureTool/MainWindow.cpp
+++ ConfigureTool/MainWindow.cpp
@@ -1,6 +1,4 @@
 #include "MainWindow.h"
-#include <QHeaderView>
-#include <QTreeWidgetItem>
 
 MainWindow::MainWindow(QWidget *parent) : QWidget(parent)
 {
@@ -9,60 +7,15 @@ MainWindow::MainWindow(QWidget *parent) : QWidget(parent)
 
 void MainWindow::InitMainWindow()
 {
-    InitMainTab();
-}
-
-void MainWindow::InitMainTab()
-{
     m_pGLayout = new QGridLayout(this);
-    m_pTabMain = new QTabWidget(this);
+    m_pTabMain = new CTabMain(this);
+
+    m_pBtnSaveSet = new QPushButton(tr("保存设置"));
+    m_pBtnSaveSet->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
+    m_pHLayout = new QHBoxLayout();
+    m_pHLayout->addWidget(m_pBtnSaveSet);
+    m_pHLayout->addSpacerItem(new QSpacerItem(20,20,QSizePolicy::Expanding,QSizePolicy::Minimum));
+
     m_pGLayout->addWidget(m_pTabMain,0,0,1,1);
-
-    m_pXML = new XML();
-    connect(m_pXML,SIGNAL(createRaderItem(QString)),this,SLOT(createRaderItem(QString)));
-    connect(m_pXML,SIGNAL(addRaderSubItem(QString)),this,SLOT(addRaderSubItem(QString)));
-    connect(m_pXML,SIGNAL(addClassItem(QString)),this,SLOT(addClassItem(QString)));
-    connect(m_pXML,SIGNAL(addItem(QString)),this,SLOT(addItem(QString)));
-    connect(m_pXML,SIGNAL(addData(QString,QString,QString)),this,SLOT(addData(QString,QString,QString)));
-
-    if(m_pXML->getAllRaderConfigDirPath("./config")){
-        m_pXML->parseConfigurationFiles();
-    }
-}
-
-void MainWindow::createRaderItem(QString strRaderName)
-{
-    m_pCurTree = new QTreeWidget(this);
-    m_pCurTree->setColumnCount(3);
-    m_pCurTree->header()->setSectionResizeMode(QHeaderView::ResizeToContents);
-    m_pCurTree->expandAll();
-    m_pCurTree->setHeaderHidden(true);
-    m_treeWidgetList.push_back(m_pCurTree);
-    m_pTabMain->addTab(m_pCurTree,strRaderName);
-}
-
-void MainWindow::addRaderSubItem(QString strSubName)
-{
-    m_pCurSubItem = new QTreeWidgetItem(m_pCurTree);
-    m_pCurSubItem->setText(0,strSubName);
-}
-
-void MainWindow::addClassItem(QString strClassName)
-{
-    m_pCurClass = new QTreeWidgetItem(m_pCurSubItem);
-    m_pCurClass->setText(0,strClassName);
-}
-
-void MainWindow::addItem(QString strName)
-{
-    m_pCurItem = new QTreeWidgetItem(m_pCurClass);
-    m_pCurItem->setText(0,strName);
-}
-
-void MainWindow::addData(QString strName,QString dataType,QString dataValue)
-{
-    QTreeWidgetItem* pData = new QTreeWidgetItem(m_pCurItem);
-    pData->setText(0,strName);
-    pData->setText(1,dataType);
-    pData->setText(2,dataValue);
+    m_pGLayout->addLayout(m_pHLayout,1,0,1,1);
 }
