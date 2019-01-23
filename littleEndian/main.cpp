764174c36339b1093627d1378f9f79527706cd6b
eddiechen <chenyouzhi06@163.com>
2019-01-23 15:55:04 +0800

    添加大小端子项目

---
+++ littleEndian/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <QCoreApplication>
+#include <QByteArray>
+#include <QDebug>
+#include <QtEndian>
+
+int isLittleEndian()
+{
+    union UCHECK
+    {
+        int n;
+        char ch;
+    }uc;
+    uc.n=1;
+    return (uc.ch==1);
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    if( isLittleEndian() )
+        qDebug()<<"Little";
+
+    if( !isLittleEndian() ){
+
+    }
+
+
+    char *aa="AB";
+    char bb[2];
+    qToBigEndian(aa,bb);
+    qDebug()<<bb;
+
+
+    return a.exec();
+}
