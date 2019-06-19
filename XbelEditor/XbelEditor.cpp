fd16593011693d78b5055bb3a27d38c09012b8de
GitHubCYZ <chenyouzhi06@163.com>
2019-06-19 17:04:59 +0800

    更新Xml解析工具

--- XbelEditor/XbelEditor.cpp
+++ XbelEditor/XbelEditor.cpp
@@ -20,14 +20,19 @@ void XbelEditor::initMainWindow( QString strCfgFilePath ){
     /*------------------------XML信息显示对象初始化--------------------------*/
     m_pXbelTree = new XbelTree(this);
     QStringList headerList;
-    headerList<<tr("属性名称")<<tr("属性值")<<tr("属性名称")<<tr("属性值")<<tr("属性名称")<<tr("属性值");
+    headerList<<QString::fromLocal8Bit("属性名称")
+            <<QString::fromLocal8Bit("属性值")
+            <<QString::fromLocal8Bit("属性名称")
+            <<QString::fromLocal8Bit("属性值")
+            <<QString::fromLocal8Bit("属性名称")
+            <<QString::fromLocal8Bit("属性值");
     m_pXbelTree->setHeaders(headerList);
     m_pXbelTree->read( strCfgFilePath );
     /*--------------------------------------------------------------------*/
 
     m_pMainLayout->addWidget(m_pXbelTree,0,0,10,7);
 
-    m_pBtnSave = new QPushButton("保存",this);
+    m_pBtnSave = new QPushButton(QString::fromLocal8Bit("保存"),this);
     m_pBtnSave->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
     connect(m_pBtnSave,SIGNAL(clicked(bool)),this,SLOT(onBtnSave()));
     m_pMainLayout->addWidget(m_pBtnSave,11,6,1,1);
