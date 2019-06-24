0507327ccbea68e47597a12d916dd97e544c6cda
GitHubCYZ <chenyouzhi06@163.com>
2019-06-24 10:20:04 +0800

    添加宏配置文件名称设置对话框

---
+++ PluginMacroConfig/FileNameDlg.cpp
@@ -0,0 +1,44 @@
+﻿#include "FileNameDlg.h"
+
+FileNameDlg::FileNameDlg():
+    m_pGLMain(nullptr),
+    m_pLEFileName(nullptr)
+{
+    m_pGLMain = new QGridLayout();
+    this->setLayout(m_pGLMain);
+    this->setFixedSize(300,150);
+    this->setObjectName("MacroFileNameDlg");
+    this->setWindowTitle(QStringLiteral("宏配置文件名称"));
+    this->setWindowFlags(Qt::WindowCloseButtonHint);
+
+    m_pLEFileName = new QLineEdit(this);
+    m_pLEFileName->setMinimumHeight(110);
+    m_pLEFileName->setPlaceholderText(QStringLiteral("请输入新增宏配置文件名称！"));
+    m_pGLMain->addWidget(m_pLEFileName,0,0,2,3);
+    m_pBtnYes = new QPushButton(QStringLiteral("确认"),this);
+    m_pBtnNo = new QPushButton(QStringLiteral("取消"),this);
+    m_pBtnYes->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
+    m_pBtnNo->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
+    connect(m_pBtnYes,SIGNAL(clicked(bool)),this,SLOT(onBtnYes()));
+    connect(m_pBtnNo,SIGNAL(clicked(bool)),this,SLOT(onBtnNo()));
+    m_pGLMain->addWidget(m_pBtnYes,2,1,1,1);
+    m_pGLMain->addWidget(m_pBtnNo,2,2,1,1);
+    m_pGLMain->setContentsMargins(5,5,5,5);
+    m_pBtnYes->setFocus();
+}
+
+QString FileNameDlg::getFileName()
+{
+    return m_pLEFileName->text();
+}
+
+void FileNameDlg::onBtnYes()
+{
+    this->accept();
+}
+
+void FileNameDlg::onBtnNo()
+{
+    this->rejected();
+    this->close();
+}
---
+++ PluginMacroConfig/FileNameDlg.h
@@ -0,0 +1,32 @@
+﻿#ifndef FILENAMEDLG_H
+#define FILENAMEDLG_H
+
+#include <QDialog>
+#include <QGridLayout>
+#include <QLineEdit>
+#include <QPushButton>
+
+class FileNameDlg : public QDialog
+{
+    Q_OBJECT
+public:
+    FileNameDlg();
+
+    QString getFileName();
+
+signals:
+
+public slots:
+
+private slots:
+    void onBtnYes();
+    void onBtnNo();
+
+private:
+    QGridLayout* m_pGLMain;
+    QLineEdit*  m_pLEFileName;
+    QPushButton* m_pBtnYes;
+    QPushButton* m_pBtnNo;
+};
+
+#endif // FILENAMEDLG_H
