9bbc66d0adf8d1dfd2ecd17b6d70de58e070bcfe
GitHubCYZ <chenyouzhi06@163.com>
2019-10-14 19:25:07 +0800

    新增日志分析归类功能

--- QHttp/QHttp/HttpCallback.cpp
+++ QHttp/QHttp/HttpCallback.cpp
@@ -64,6 +64,149 @@ void HttpCallback::updateDict(int nClientId, QString strAction, QString strIP)
     }
 }
 
+void HttpCallback::updateClientIdList(int nClientId)
+{
+    auto it=m_ClientIdSet.find(nClientId);
+    if( it==m_ClientIdSet.end() ){
+        m_ClientIdSet.insert(nClientId);
+        m_ClientIdList.push_back(nClientId);
+    }
+}
+
+void HttpCallback::updateClientActionSet(int nClientId, QString strAction)
+{
+    auto itMap=m_ClientActionSet.find(nClientId);
+    if( itMap==m_ClientActionSet.end() ){
+        QSet<QString> actionSet;
+        actionSet.insert(strAction.trimmed());
+        m_ClientActionSet.insert(nClientId,actionSet);
+    }
+    else{
+        auto itSet=itMap.value().find(strAction.trimmed());
+        if( itSet==itMap.value().end() ){
+            itMap.value().insert(strAction.trimmed());
+        }
+    }
+}
+
+void HttpCallback::updateClientUpDownMap(QString strAction, int nClientId)
+{
+    auto itMap=m_ClientActionSet.find(nClientId);
+    if( itMap!=m_ClientActionSet.end() ){
+        //判断推拉流以及协议类型
+        int nSetSz=itMap.value().size();
+        if( nSetSz>0 ){
+            auto itSet=itMap.value().find(strAction.trimmed());
+            if( nSetSz==1 ){
+                if( itSet!=itMap.value().end() && strAction.trimmed()=="on_play"){
+                    m_ClientUpDownMap.insert(nClientId,"pull");
+                }
+            }
+            else if(  nSetSz==2 ){
+                auto itConnect=itMap.value().find("on_connect");
+                if( itConnect!=itMap.value().end() ){
+                    if( strAction.trimmed()=="on_play" )
+                        m_ClientUpDownMap.insert(nClientId,"pull");
+                    else if( strAction.trimmed()=="on_publish" )
+                        m_ClientUpDownMap.insert(nClientId,"push");
+                    else
+                         m_ClientUpDownMap.insert(nClientId,"Unknow");
+                }
+            }
+        }
+    }
+}
+
+void HttpCallback::updateClientActionMap(int nClientId, QString strAction, QString strIP)
+{
+    QString strTime=QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss.zzz");
+    QMap<int,QList<ClientAction>>::iterator it=m_ClientActionMap.find(nClientId);
+    if( it==m_ClientActionMap.end() ){
+        QList<ClientAction> caList;
+        ClientAction clientAction;
+        clientAction.strAction=strAction.trimmed();
+        clientAction.strTime=strTime;
+        clientAction.strIP=strIP;
+        caList.push_back(clientAction);
+        m_ClientActionMap.insert(nClientId,caList);
+    }
+    else{
+        bool bFind=false;
+        for( auto itList=it.value().begin();itList!=it.value().end();itList++ ){
+            if( (*itList).strAction==strAction.trimmed() ){
+                (*itList).strTime=strTime;
+                bFind=true;
+            }
+        }
+        if( !bFind ){
+            ClientAction clientAction;
+            clientAction.strAction=strAction.trimmed();
+            clientAction.strTime=strTime;
+            clientAction.strIP=strIP;
+            it.value().push_back(clientAction);
+        }
+    }
+}
+
+void HttpCallback::updateClientProtocolType(int nClientId, QString strAction)
+{
+    auto itMap=m_ClientActionSet.find(nClientId);
+    if( itMap!=m_ClientActionSet.end() ){
+        int nSetSz=itMap.value().size();
+        if( nSetSz>0 ){
+            auto itSet=itMap.value().find(strAction.trimmed());
+            if( nSetSz==1 ){
+                if( itSet!=itMap.value().end() && strAction.trimmed()=="on_play"){
+                    m_ClientProtocolType.insert(nClientId,"http-flv");
+                }
+            }
+            else if(  nSetSz==2 ){
+                auto itConnect=itMap.value().find("on_connect");
+                if( itConnect!=itMap.value().end() ){
+                    if( strAction.trimmed()=="on_play" || strAction.trimmed()=="on_publish" )
+                        m_ClientProtocolType.insert(nClientId,"rtmp");
+                    else
+                        m_ClientProtocolType.insert(nClientId,"Unknow");
+                }
+            }
+        }
+    }
+}
+
+void HttpCallback::outputLog()
+{
+    //ID
+    for( auto it=m_ClientIdList.begin();it!=m_ClientIdList.end();it++ ){
+        int nClientID=(*it);
+        //qDebug()<<"A";
+        //推拉流
+        auto udIt=m_ClientUpDownMap.find(nClientID);
+        if( udIt!=m_ClientUpDownMap.end() ){
+            //qDebug()<<"B";
+            //协议类型
+            auto ptIt=m_ClientProtocolType.find(nClientID);
+            if( ptIt!=m_ClientProtocolType.end() ){
+                //事件
+                //qDebug()<<"C";
+                auto actIt=m_ClientActionMap.find(nClientID);
+                if( actIt!=m_ClientActionMap.end() ){
+                    //qDebug()<<"D";
+                    for(QList<ClientAction>::iterator itList=actIt.value().begin();itList!=actIt.value().end();itList++){
+                        qDebug("ID:%d UpDown:%s ProtocolType:%9s Time:%s IP:%s Action:%s",
+                               nClientID,
+                               udIt.value().toLocal8Bit().data(),
+                               ptIt.value().toLocal8Bit().data(),
+                               (*itList).strTime.toLocal8Bit().data(),
+                               (*itList).strIP.toLocal8Bit().data(),
+                               (*itList).strAction.toLocal8Bit().data());
+                    }
+                }
+            }
+        }
+    }
+    qDebug()<<"\n";
+}
+
 void HttpCallback::onServerStarted (quint16 port) {
     //qDebug () << "QtHttpServer started on port" << port << server->getServerName ();
 }
@@ -93,7 +236,13 @@ void HttpCallback::onRequestNeedsReply (QtHttpRequest * request, QtHttpReply * r
         int nClientId=reqJsonObj.value("client_id").toInt();
         QString strIP=reqJsonObj.value("ip").toString();
 
-        updateDict(nClientId,strAction,strIP);
+        //updateDict(nClientId,strAction,strIP);
+        updateClientIdList(nClientId);
+        updateClientActionMap(nClientId,strAction,strIP);
+        updateClientActionSet(nClientId,strAction);
+        updateClientUpDownMap(strAction,nClientId);
+        updateClientProtocolType(nClientId,strAction);
+        outputLog();
     }
 
     QJsonObject json;
--- QHttp/QHttp/HttpCallback.h
+++ QHttp/QHttp/HttpCallback.h
@@ -9,6 +9,14 @@
 #include "QtHttpHeader.h"
 
 #include <QMap>
+#include <QSet>
+#include <QList>
+
+struct ClientAction{
+    QString strAction;
+    QString strTime;
+    QString strIP;
+};
 
 class HttpCallback : public QObject {
     Q_OBJECT
@@ -19,6 +27,13 @@ public:
 
 private:
     void updateDict(int nClientId,QString strAction,QString strIP);
+    void updateClientIdList(int nClientId);
+    void updateClientActionSet(int nClientId,QString strAction);
+    void updateClientUpDownMap(QString strAction="Unknow",int nClientId=0);
+    void updateClientActionMap(int nClientId,QString strAction,QString strIP);
+    void updateClientProtocolType(int nClientId,QString strAction);
+
+    void outputLog();
 
 public slots:
     void onServerStopped      (void);
@@ -33,6 +48,14 @@ private:
 
     //<clientId,<action,IP+Time>>
     QMap<int,QMap<QString,QString>>m_dict;
+
+    QSet<int>m_ClientIdSet;//Id集合
+    QList<int>m_ClientIdList;//记录所有Id(有序)
+    QMap<int,QSet<QString>>m_ClientActionSet;//记录当前ID所接收到的事件
+    //QMultiMap<QString,int>m_ClientUpDownMap;//推拉流Map
+    QMap<int,QString>m_ClientUpDownMap;
+    QMap<int,QList<ClientAction>>m_ClientActionMap;//客户端事件
+    QMap<int,QString>m_ClientProtocolType;//客户端协议类型
 };
 
 #endif // HTTPCALLBAK_H
